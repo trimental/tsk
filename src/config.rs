d0a41f5eed9148666b84e4ac3513dacafdb86131
Lucas Timmins <timmins.s.lucas@gmail.com>
2018-10-02 18:29:02 +0800

    Simply config loading and creating logic

--- src/config.rs
+++ src/config.rs
@@ -24,29 +24,22 @@ impl TskConfig {
     pub fn new() -> TskConfig {
         let xdg_dirs = xdg::BaseDirectories::with_prefix("tsk").unwrap();
         let location = xdg_dirs.place_config_file("config.toml").unwrap();
-
-        let new = location.exists();
-        OpenOptions::new()
-            .create(true)
-            .read(true)
-            .write(true)
-            .open(location.clone())
-            .unwrap();
-
+        
         let mut tsk_config = TskConfig {
-            location,
+            location: location.clone(),
             config: Config::new(),
         };
 
-        // If its a new config file then write the default config to it
-        if new {
+        if location.exists() {
+            tsk_config.read();
+        } else {
             tsk_config.write();
         }
 
         tsk_config
     }
 
-    pub fn read(&mut self) -> Config {
+    pub fn read(&mut self) {
         let mut config_file = OpenOptions::new()
             .read(true)
             .write(true)
@@ -56,11 +49,12 @@ impl TskConfig {
         config_file
             .read_to_string(&mut buf)
             .expect("Could not read config");
-        toml::from_str(&buf).expect("Could not parse config")
+        self.config = toml::from_str(&buf).expect("Could not parse config")
     }
 
     pub fn write(&mut self) {
         let mut config_file = OpenOptions::new()
+            .create(true)
             .read(true)
             .write(true)
             .open(self.location.clone())
